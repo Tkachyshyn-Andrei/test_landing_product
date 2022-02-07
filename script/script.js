949212985d2c3aab228363801351d645304e772a
Tkachyshen Andrei <tkachyshen.a@gmail.com>
2022-02-07 23:40:58 +0200

    fix modal register

--- script/script.js
+++ script/script.js
@@ -16,18 +16,18 @@ $(document).ready(function () {
 
 // прокрутка
 
-$(document).ready(function () {
-    $('[href^="#"]').on('click', function (event) {
-        if ($(this).attr('hash') !== "") {
-            event.preventDefault();
-            let hash = $(this).prop('hash');
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 3000, function () {
-            });
-        }
-    });
+
+$('[href^="#"]').on('click', function (event) {
+    if ($(this).attr('hash') !== "") {
+        event.preventDefault();
+        let hash = $(this).prop('hash');
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 3000, function () {
+        });
+    }
 });
+
 
 // Ефект нахилу картки
 VanillaTilt.init(document.querySelectorAll(".card_price"), {
@@ -94,38 +94,51 @@ btn.addEventListener('click', () => {
 });
 
 // modal registration
-$(".sign_up").on('click', function (){
+$(".sign_up").on('click', function () {
     Swal.fire({
         title: 'Registration',
         html:
             `<input type="text" id="login" class="swal2-input" placeholder="Username">              
-            <input type="email" id="email" class="swal2-input" placeholder="Enter email address">
+            <input type="email" id="email" class="swal2-input" placeholder="Enter email address">            
             <input type="text" id="phone" class="swal2-input" placeholder="Enter phone number">
-            <input type="number" id="birthday" class="swal2-input" placeholder="Enter date of birth">   
+            
+            <input class="swal2-input flatpickr-input" id="expiry-date" placeholder="Enter date of birth" type="text" readonly="readonly">
+            
             <select id="country" class="swal2-select"">
             <option value="" disabled="">Select a country</option> 
-            <option value="bananas">Ukraine</option>    
-            <input class="swal2-input flatpickr-input" id="expiry-date" type="text" readonly="readonly">
+            <option value="bananas">Ukraine</option>   
             `,
         confirmButtonText: 'Sign in',
         focusConfirm: false,
+
         preConfirm: () => {
             const login = Swal.getPopup().querySelector('#login').value
             const email = Swal.getPopup().querySelector('#email').value
             const phone = Swal.getPopup().querySelector('#phone').value
-            const birthday = Swal.getPopup().querySelector('#birthday').value
             const country = Swal.getPopup().querySelector('#country').value
-            if (!login || !email || !phone || !birthday || !country) {
+            const emailReg = /^([\w-.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+            if (!emailReg.test( email ) ) {
+                Swal.showValidationMessage(`The email should be in the format: email@domain.com`)
+            } else
+            if (flatpickrInstance.selectedDates[0] > new Date()) {
+                Swal.showValidationMessage(`Please enter a valid date of birth`)
+            }else
+            if (!login || !email || !phone || !country) {
                 Swal.showValidationMessage(`Please fill in all fields`)
             }
-            return { login: login, email: email, phone: phone, birthday: birthday}
+            return {login: login, email: email, phone: phone}
+        },
+        willOpen: () => {
+            flatpickrInstance = flatpickr(
+                Swal.getPopup().querySelector('#expiry-date')
+            )
         },
         showClass: {
             popup: 'animate__animated animate__fadeInDown'
         },
         hideClass: {
             popup: 'animate__animated animate__fadeOutUp'
-        }
+        },
     }).then((result) => {
         if (result.isConfirmed) {
             Swal.fire(
